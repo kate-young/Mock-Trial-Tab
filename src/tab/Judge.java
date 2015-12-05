723d66422ed03bd09bd84e343af204723bd09381
Kate Young <S1391628@student.mcckc.edu>
2015-12-05 13:29:52 -0600

    Build out Judge class

--- src/tab/Judge.java
+++ src/tab/Judge.java
@@ -1,4 +1,31 @@
 package tab;
 
+import java.util.ArrayList;
+
 public class Judge {
+	private final String firstName;
+	private final String lastName;
+	private ArrayList<School> conflicts;
+	
+	public Judge(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Judge(String firstName, String lastName, ArrayList<School> conflicts) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.conflicts = conflicts;
+	}
+	public void addConflict(School conflict) {
+		this.conflicts.add(conflict);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
 }
